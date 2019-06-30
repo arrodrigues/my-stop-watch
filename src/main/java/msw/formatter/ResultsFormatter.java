64ac8c10671ee0a7bd28dc43892645050e9903ba
Antonio Rafael Rodrigues <antonio.rafael.ar@gmail.com>
2019-06-30 17:21:31 -0300

    Header Line at the formatter

--- src/main/java/msw/formatter/ResultsFormatter.java
+++ src/main/java/msw/formatter/ResultsFormatter.java
@@ -9,14 +9,15 @@ import java.util.stream.IntStream;
 public class ResultsFormatter {
     public static final String TAB = "   ";
     public static final String SEPARATOR = "   ";
-    private final List<ResultNode> nodes;
+    public static final int HEADER_LINE_SEPARATOR_SIZE = 40;
+    public static final String HEADER_LINE_SEPARATOR_CHAR = "-";
 
-    public ResultsFormatter(List<ResultNode> nodes) {
-        this.nodes = nodes;
+    public ResultsFormatter() {
     }
 
-    public String format() {
+    public String format(List<ResultNode> nodes) {
         StringBuilder sb = new StringBuilder();
+        appendHeader(sb);
         format(sb, nodes, 0);
         return sb.toString();
     }
@@ -25,10 +26,19 @@ public class ResultsFormatter {
         for (ResultNode resultNode : nodes) {
             IntStream.range(0, level).forEach(l -> sb.append(TAB));
             sb.append(resultNode.getTask())
-                    .append(SEPARATOR).append(resultNode.getIntervalTotal())
-                    .append(SEPARATOR).append(resultNode.getIntervalExcludingSubnodes());
+                    .append(SEPARATOR).append(resultNode.getIntervalExcludingSubnodes())
+                    .append(SEPARATOR).append(resultNode.getIntervalTotal());
             sb.append(System.lineSeparator());
-            format(sb, resultNode.getResultNodes(), level+1);
+            format(sb, resultNode.getResultNodes(), level + 1);
         }
+    }
+
+    private void appendHeader(StringBuilder sb) {
+        sb.append("Task")
+                .append(SEPARATOR).append("Interval")
+                .append(SEPARATOR).append("Interval+Subnodes")
+                .append(System.lineSeparator());
+        IntStream.rangeClosed(0, HEADER_LINE_SEPARATOR_SIZE).forEach(n -> sb.append(HEADER_LINE_SEPARATOR_CHAR));
+        sb.append(System.lineSeparator());
     }
 }
--- src/test/java/msw/results/ResultsFormatterTest.java
+++ src/test/java/msw/results/ResultsFormatterTest.java
@@ -4,7 +4,9 @@ import msw.formatter.ResultsFormatter;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ResultsFormatterTest {
 
     @Test
-    public void formt1(){
+    public void format(){
         List<ResultNode> nodes = new ArrayList<>();
 
         ResultNode node = new ResultNode("Task1", 100, 200);
@@ -22,12 +24,20 @@ public class ResultsFormatterTest {
                 .getResultNodes()
                 .add(new ResultNode("Task1.1", 110, 150));
 
-        ResultsFormatter formatter = new ResultsFormatter(nodes);
+        ResultsFormatter formatter = new ResultsFormatter();
+        String formatted = formatter.format(nodes);
+        String format = removeTheFirstTwoLines(formatted);
+        assertThat(format).isEqualTo(
+                 "Task1   60   100\n"+
+                 "   Task1.1   40   40");
 
-        assertThat(formatter.format()).isEqualTo(
-                 "Task1  100   60\n"+
-                 "   Task1.1  40   40\n");
+    }
 
+    private String removeTheFirstTwoLines(String format) {
+        return Arrays
+                .stream(format.split(System.lineSeparator()))
+                .skip(2)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 
 }
